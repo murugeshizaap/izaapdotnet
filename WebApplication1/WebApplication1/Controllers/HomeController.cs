2b834a9d00e9db241f05c538c624dbf23ac900fe
murugeshizaap <100023051+murugeshizaap@users.noreply.github.com>
2022-02-19 18:23:04 +0530

    test

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -11,8 +11,7 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             return View();
-
-            //
+            
         }
 
         public ActionResult About()
